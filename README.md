d73db480c7e15564c6e783304834e97e865f9618
Daniel T Citron <dtc65@cornell.edu>
2020-10-04 17:30:13 -0700

    Tidied up the baseline configuration

--- README.md
+++ README.md
@@ -4,24 +4,24 @@ This repository contains the code and documentation which support the data analy
 
 The supporting data which accompanies this code may be found *here*. *Need appropriate citations*
 
-  * `aggregated_2015_2018_travel_data.csv` - contains population census data; trips recorded between 2015-2018 from the BIMEP MIS
-  * `trip_duration_data.csv` - contains data describing the duration of trips to different destinations from the 2018 BIMEP MIS
-  * `travel_dist_by_region.csv` - contains distances between each areaId and the centroid of each destination region
-  * `PfPR_draws.csv` - Draws from the joint posterior surface of malaria prevalence estimates *Need appropriate citations*
-  * `data/clean/negative_binomial_predictions_by_destination_region.csv` - Trip destination model results
-  * `trip_frequency_model_estimates.csv` - Frequency of travel model results
+  * `aggregated_2015_2018_travel_data.csv` — contains population census data; trips recorded between 2015-2018 from the BIMEP MIS
+  * `trip_duration_data.csv` — contains data describing the duration of trips to different destinations from the 2018 BIMEP MIS
+  * `travel_dist_by_region.csv` — contains distances between each areaId and the centroid of each destination region
+  * `PfPR_draws.csv` — Draws from the joint posterior surface of malaria prevalence estimates *Need appropriate citations*
+  * `data/clean/negative_binomial_predictions_by_destination_region.csv` — Trip destination model results
+  * `trip_frequency_model_estimates.csv` — Frequency of travel model results
 
 The R code inside the **scripts** folder is used to preprocess and analyze the data.
 
-  * `Trip_frequency_model.R` --- Estimates the frequency with which residents leave home. 
-  * `Trip_destination_choice_model.R ` --- Estimates the probability distribution of choosing a destination region, in a manner similar to a gravity model.
-  * `Trip_duration_model.R` --- Fits an exponential model to the time spent away
-  * `region_to_areaId_mapping.R` --- A script for disaggregating trips to regions into trips to specific map-areas within each region. We assume that the probability of visiting each map-area is proportional to population.
+  * `Trip_frequency_model.R` — Estimates the frequency with which residents leave home. 
+  * `Trip_destination_choice_model.R ` — Estimates the probability distribution of choosing a destination region, in a manner similar to a gravity model.
+  * `Trip_duration_model.R` — Fits an exponential model to the time spent away
+  * `region_to_areaId_mapping.R` — A script for disaggregating trips to regions into trips to specific map-areas within each region. We assume that the probability of visiting each map-area is proportional to population.
 
-The R code insde the **simulation_configurations** folder serves as example code which maybe used to generate and plot the results from a single simulation run for each of the different scenarios discussed in the article.
+The R code insde the **simulation_configurations** folder serves as example code which maybe used to generate and plot the results from a single simulation run for each of the different scenarios discussed in the article. The software we use to build and run the simulations is called `macro.pfsi`, and is available as a package through GitHub, with instructions for how to install [here](https://github.com/dd-harp/MASH).
 
-  * `baseline_simulation_configuration.R` --- Baseline case, assuming that conditions do not change on Bioko Island
-  * `local_residual_configuration.R` --- Estimating the local residual fraction, the fraction of cases that are attributable to on-island local transmission only, by reducing the force of infection on the mainland to zero.
-  * `travel_fraction_configuration.R` --- Estimating the fraction of PfPR which is attributable to travel, by reducing the on-island local transmission to zero.
-  * `vaccination_configuration.R` --- Estimating the long-term impact of a vaccine on malaria prevalence
-  * `travel_vaccination_configuration.R` --- Estimating the impact of vaccinating and treating travelers to the mainland.
+  * `baseline_simulation_configuration.R` — Baseline case, assuming that conditions do not change on Bioko Island
+  * `local_residual_configuration.R` — Estimating the local residual fraction, the fraction of cases that are attributable to on-island local transmission only, by reducing the force of infection on the mainland to zero.
+  * `travel_fraction_configuration.R` — Estimating the fraction of PfPR which is attributable to travel, by reducing the on-island local transmission to zero.
+  * `vaccination_configuration.R` — Estimating the long-term impact of a vaccine on malaria prevalence
+  * `travel_vaccination_configuration.R` — Estimating the impact of vaccinating and treating travelers to the mainland.
--- simulation_configurations/baseline_simulation_configuration.R
+++ simulation_configurations/baseline_simulation_configuration.R
@@ -1,18 +1,8 @@
 ##
 #
 # Script for Outputting Baseline Bioko Island Simulations
-# For ASTMH
-# Propagating through spatial uncertainty 
 #
 # Daniel T Citron
-# 10/29/19
-#
-##
-#
-# A single run of the simulation
-# Designed to be called using a qsub job on the cluster
-# Pass in a random seed, which will serve as the seed for the simulation.
-# The random seed will also serve as a seed for drawing randomly from 1 to 100 surfaces.
 #
 ##
 
@@ -20,23 +10,22 @@ rm(list=ls());gc()
 
 # Load Libraries ####
 library(data.table)
-library(ggplot2)
 library(Matrix)
 library(MASS)
 library(here)
 
-# Load macro.pfsi
+# Load macro.pfsi, library for simulations
+library(Rcpp)
+library(RcppArmadillo)
+library(RcppProgress)
+# If not installed:
+#devtools::install_github(repo = "https://github.com/dd-harp/MASH",subdir = "macro.pfsi")
 library(macro.pfsi)
 
-# Set random seed for drawing the surface from the joint posterior distribution ####
-set.seed(1)
-# Index of the surface we draw from
-pfpr.draw.ix <- sample(1:100, 1)
-
-# Load Data ####
+# Load Data and Set Parameter Values ####
 # population data
 pop.data <- fread(here("data/clean/aggregated_2015_2018_travel_data.csv"))
-# travel frequency
+# travel frequencies
 trip.freq.data <- fread(here("data/clean/trip_frequency_model_estimates.csv"))
 # travel destination selection model
 trip.dest.data <- fread(here("data/clean/negative_binomial_predictions_by_destination_region.csv")) 
@@ -54,7 +43,7 @@ pfpr.data <- fread(here("data/clean/pfpr_draws.csv"))
 pfpr.data <- merge(pfpr.data, pop.data[year == 2018, .(areaId)], by = "areaId", all = FALSE)
 
 
-# Dynamical Model Parameters ####
+# Dynamical Model Parameters
 a = 0.3*0.9
 b = 0.55
 c = 0.15
@@ -66,7 +55,9 @@ peip = p^11 # fraction of mosquitoes who survive incubation period
 rho = FeverPf*TreatPf # probability of clearing infection through treatment cascade
 
 
-# TaR Matrix ####
+# Define Time Spent at Risk Matrix
+# Used for calibrating PfPR across the model
+#
 # get the region - to - pixel matrix
 source(here("scripts/region_to_areaId_mapping.R"))
 # convert the trip.dest.data into a matrix
@@ -76,39 +67,42 @@ trip.dest.mat <- rbind(trip.dest.mat, matrix(c(1,0,0,0,0,0,0), ncol = 7))
 # take the matrix product of trip.dest.mat with the region-to-pixel matrix toget pixel-to-pixel:
 movement.matrix <- trip.dest.mat %*% reg.2.pixel
 
-# NB: the movement matrix is the probability of going from each areaId to each other areaId;
-# we need to combine it with the other travel data to obtain the full TaR matrix
-TaR.matrix <- diag(1, nrow = 242, ncol = 242)
-# vector of trip durations across the island and off-island
+# Trip durations, by destination across the island and off-island
 trip.duration <- c(rep(trip.duration.bi, 241), trip.duration.eg)
-# vector of frequencies at which people leave home
-# where we divide by 56 to transform the probability of leaving into 
-# the frequency of leaving during the study period
 
+# Frequencies at which people leave home
+# Divide by 56 days to transform the probability of leaving into 
+# the frequency of leaving during the 8-week study period
 freq.draw.ix = paste0("draw.","mean")
 trip.freq <- trip.freq.data[year == 2018, c("areaId", freq.draw.ix), with = FALSE]
 colnames(trip.freq)[2] <- "freq"
 trip.freq <- c(trip.freq[order(areaId)]$freq/56, 1)
+
+# Build full Time Spent at Risk matrix
+TaR.matrix <- diag(1, nrow = 242, ncol = 242)
 for (i in 1:242){
   TaR.matrix[i,] <- (movement.matrix[i,]*trip.duration)/(sum(movement.matrix[i,]*trip.duration) + 1/trip.freq[i])
   TaR.matrix[i,i] <- 1 - sum(TaR.matrix[i,])
   TaR.matrix[i,] <- TaR.matrix[i,]/sum(TaR.matrix[i,])
 }
 
-
 # Derive EIR ####
+# Set random seed for drawing the surface from the joint posterior distribution #
+set.seed(1)
+pfpr.draw.ix <- sample(1:100, 1)
+# Draw from PfPR surface
 pfpr.draw.colname = paste0("draw.", pfpr.draw.ix)
 pfpr.data.draw = pfpr.data[,c("areaId", pfpr.draw.colname), with = FALSE]
 colnames(pfpr.data.draw)[2] = "pfpr"
 pop.data <- merge(pop.data[year == 2018], pfpr.data.draw, by = "areaId")
 pfpr.input <- c(pop.data$pfpr, 0.43)
 
+# See Ruktanonchai et al. (2016) and Supplementary Information for derivation
 odds.vector <- r/(1-rho)*pfpr.input/(1-(1+rho*r/eta/(1-rho))*pfpr.input)
 h.FOI <- MASS::ginv(TaR.matrix) %*% odds.vector
 h.FOI[which(h.FOI < 0)] <- 0
 
-
-# Derive Initial Conditions for Mosquitoes ####
+# Calculate Mosquito Parameters ####
 # we begin by calculating the fraction of infected mosquitoes, the sporozoite rate, from the Ross-Macdonald equations + Kappa
 # Total population, including visitors
 H.visitors <- t(c(pop.data[year == 2018]$pop, 0) %*% TaR.matrix)
@@ -116,6 +110,7 @@ H.visitors <- t(c(pop.data[year == 2018]$pop, 0) %*% TaR.matrix)
 X.visitors <- t((c(pop.data[year == 2018]$pop, 0) * pfpr.input)  %*% TaR.matrix)
 # This is the number of people who are sick, including visitors and residents both
 kappa <- X.visitors/H.visitors
+# Define kappa off-island, based on PfPR off-island
 kappa[242] <- .43
 z.spz <- peip*a*c*kappa/(p*a*c*kappa + (1-p))
 # this is Z/M, but we currently do not know M
@@ -126,18 +121,14 @@ Z[242] = 0 # for off-island
 Y = Z/peip
 Y[242] = 0 # for off-island
 
-
-# Derive Lambda ####
-# We use this quantity to set the emergence rate of mosquitoes in each of the patches
+# Derive Lambda, set the emergence rate of mosquitoes in each of the patches
 # this is Lambda, calculated based on equilibrium value for the emergence process
 Lambda = M*(1-p)/p
 Lambda[242] = 0 # for off-island
 
-
 # PfSI Parameters ####
-# vector of parameters 
+# Parameters definie probabilities of symptomatic malaria and treatment seeking
 pfsi_pars <- pfsi_parameters(FeverPf = 0.1116336, TreatPf = 0.602)
-
 
 # Patch Parameters ####
 # set up patches (n is how many patches we have)
@@ -152,8 +143,9 @@ patch_pars <- patches_parameters(move = movement.matrix,
                                  res_EIR = c(rep(0,(n.patch-1)),eg.eir)
 )
 
-
-# Mosy Parameters ####
+# Mosquito Parameters ####
+# After setting up the patches, we populate them with mosquitoes 
+# and define the emergence rate of mosquitoes in each patch
 psi <- Matrix::sparseMatrix(i = {},j = {},x = 0.0,dims = c(n.patch,n.patch))
 diag(psi) <- rep(1,n.patch)
 lambda.matrix = t(matrix(Lambda, nrow = n.patch, ncol = 365))
@@ -167,15 +159,14 @@ mosy_pars <- mosquito_rm_conpars(N = n.patch,
 
 
 # Human Parameters ####
-# number of people in each patch
+# After setting up the patches, we populate them with people
 patch.human.pop <- c(pop.data[year == 2018]$pop, 0) 
-# malaria prevalence in each patch
-pfpr <- pfpr.input
+
 # total number of humans
 n.humans <- sum(patch.human.pop) 
 
-# sample S or I for each person
-# set seed first, for the sake of reproducibility - this is the same as the seed passed to this script
+# Randomly set initial infection status of each human host malaria prevalence in each patch
+pfpr <- pfpr.input
 set.seed(1)
 init_state <- unlist(
   mapply(FUN = function(n,pr){
@@ -190,8 +181,8 @@ init_state <- unlist(
 # These IDs correspond to indices in a vector - need it to be 0-indexed for c++
 patch_id <- rep(0:(n.patch-1), times=patch.human.pop)
 
-# Set uniform biting weights; this could follow any density on the positive reals (gamma, log-normal, weibull, etc.)
-bweights <- rep(1,n.humans)
+# Set uniform biting weights
+bweights <- rep(1, n.humans)
 
 # Set mean trip durations based on destination
 trip.durations <- c(rep(trip.duration.bi, 241), trip.duration.eg)
@@ -199,8 +190,8 @@ trip.durations <- c(rep(trip.duration.bi, 241), trip.duration.eg)
 trip.freqs <- trip.freqs <- rep(trip.freq[1:241], times = patch.human.pop[1:241])
 
 
-# the data structure that will be passed down to C++ to construct the human pop
-human_pars <- vector("list",n.humans)
+# The data structure for constructing the human pop
+human_pars <- vector("list", n.humans)
 for(i in 1:n.humans){
   human_pars[[i]] <- human_pfsi_conpars(id = i-1,
                                         home_patch_id = patch_id[i],
@@ -212,8 +203,9 @@ for(i in 1:n.humans){
                                         bite_algorithm = 0)
 }
 
+# Run Simulation ####
 
-# Define Path Names ####
+# Define Output Path Names #
 log_pars <- list()
 h_inf <- here("data/simulation_outputs", paste0("pfsi_", 1, ".csv"))
 log_pars[[1]] <- list(outfile = h_inf, 
@@ -236,10 +228,7 @@ log_pars[[2]] <- list(outfile = mosy,
                                         paste0("patch",1:n.patch)),
                                       collapse = ","))
 
-
-# Run Simulation ####
-# Set random seed, for reproducibility
-# Use the seed passed to this program
+# Set random seed
 set.seed(1)
 # 
 run_macro(tmax = 7*365,
@@ -252,7 +241,8 @@ run_macro(tmax = 7*365,
           verbose = T)
 
 
-# Analyzing the output - 
+# Analyzing the output ####
+library(ggplot2)
 
 h_inf <- here("data/simulation_outputs", paste0("pfsi_", 1, ".csv"))
 dt <- fread(h_inf)
