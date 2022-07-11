3f56ee478b8c2819e35afe53795b09d9afac66c5
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-07-11 16:44:56 -0700

    check 3 patch equilibrium works

--- MicroMoB_config/test_bm.R
+++ MicroMoB_config/test_bm.R
@@ -4,51 +4,6 @@ library(progress)
 library(ggplot2)
 library(deSolve)
 
-# --------------------------------------------------------------------------------
-# figuring out how to set up the equilibrium model in 3 patch case
-# --------------------------------------------------------------------------------
-
-# TaR <- matrix(
-#   data = c(
-#     0.9, 0.025, 0.075,
-#     0.1, 0.85, 0.05,
-#     0.01, 0.09, 0.9
-#   ),
-#   nrow = 3, ncol = 3, byrow = TRUE
-# )
-# 
-# # pfpr amongst residents
-# pfpr <- c(0.4, 0.25, 0.1)
-# 
-# # total human pop
-# H <- c(100, 200, 300)
-# 
-# 
-# 
-# 
-# 
-# # See Ruktanonchai et al. (2016) and Supplementary Information for derivation
-# odds.vector <- r/(1-rho)*pfpr/(1-(1+rho*r/eta/(1-rho))*pfpr)
-# 
-# # Force of Infection (FOI) or "happenings rate" h
-# h.FOI <- MASS::ginv(TaR.matrix) %*% odds.vector
-# h.FOI[which(h.FOI < 0)] <- 0
-# h.FOI <- as.vector(h.FOI)
-# 
-# # Calculate Mosquito Parameters ####
-# # we begin by calculating the fraction of infected mosquitoes, the sporozoite rate, from the Ross-Macdonald equations + Kappa
-# # Total population, including visitors
-# H.visitors <- t(pop.data[year == 2018, pop] %*% TaR.matrix)
-# # Sick population, including visitors
-# X.visitors <- t((pop.data[year == 2018, pop] * pfpr)  %*% TaR.matrix)
-# 
-# # kappa: net infectiousness of human pop at each place
-# kappa <- X.visitors/H.visitors * c
-# kappa <- as.vector(kappa)
-# 
-# H.visitors <- as.vector(H.visitors)
-# n_patch <- length(kappa)
-
 
 # --------------------------------------------------------------------------------
 # test SIP equilibrium for non spatial model
@@ -74,35 +29,35 @@ r = 1/200 # rate at which people become cured
 eta = 1/32 # rate at which prophylaxis wears off
 rho = FeverPf*TreatPf # probability of clearing infection through treatment cascade
 
-pfpr <- 0.2
-N <- 500
-
-# eq equations for ODE
-I <- pfpr * N
-S <- N - I - ((rho*I*r)/(eta*(1-rho)))
-P <- N - I - S
-h <- (eta * P) / (rho * S)
-
-sip_ode <- function(t, y, params) {
-  eta <- params$eta
-  r <- params$r
-  h <- params$h
-  rho <- params$rho
-  dxdt <- rep(0, 3)
-  S <- y[1]
-  I <- y[2]
-  P <- y[3]
-  dxdt[1] <- -h*S + r*I + eta*P
-  dxdt[2] <- h*(1-rho)*S - r*I
-  dxdt[3] <- h*rho*S - eta*P
-  return(list(dxdt))
-}
-
-params <- list(r=r,h=h,rho=rho,eta=eta)
-y <- c(S,I,P)
-
-out <- ode(y = y, times = 0:100, func = sip_ode, parms = params, method = "ode23")
-plot(out)
+# pfpr <- 0.2
+# N <- 500
+# 
+# # eq equations for ODE
+# I <- pfpr * N
+# S <- N - I - ((rho*I*r)/(eta*(1-rho)))
+# P <- N - I - S
+# h <- (eta * P) / (rho * S)
+# 
+# sip_ode <- function(t, y, params) {
+#   eta <- params$eta
+#   r <- params$r
+#   h <- params$h
+#   rho <- params$rho
+#   dxdt <- rep(0, 3)
+#   S <- y[1]
+#   I <- y[2]
+#   P <- y[3]
+#   dxdt[1] <- -h*S + r*I + eta*P
+#   dxdt[2] <- h*(1-rho)*S - r*I
+#   dxdt[3] <- h*rho*S - eta*P
+#   return(list(dxdt))
+# }
+# 
+# params <- list(r=r,h=h,rho=rho,eta=eta)
+# y <- c(S,I,P)
+# 
+# out <- ode(y = y, times = 0:100, func = sip_ode, parms = params, method = "ode23")
+# plot(out)
 
 
 # --------------------------------------------------------------------------------
@@ -207,29 +162,23 @@ lambda <- g*M
 # bm$kappa <- as.vector(t(bm$beta) %*% (bm$x*bm$H))
 
 
-
-
-
-
 # --------------------------------------------------------------------------------
 #   deterministic simulation
 # --------------------------------------------------------------------------------
 
 # initial conditions
-n_patch <- 3
+n_patch <- nrow(TaR)
 human_state <- matrix(data = 0, nrow = n_patch, ncol = 3, dimnames = list(NULL, c('S', 'I', 'P')))
 human_state[, 'S'] <- S
 human_state[, 'I'] <- I
 human_state[, 'P'] <- P
 
-tmax <- 365 * 2
+tmax <- 365 * 10
 mod <- make_MicroMoB(tmax = tmax, p = n_patch)
 
 setup_aqua_trace(model = mod, lambda = lambda, stochastic = FALSE)
-setup_mosquito_RM(model = mod, stochastic = FALSE, f = f, q = q, eip = eip, p = p, psi = diag(3), nu = 25, M = M, Y = Y, Z = Z)
+setup_mosquito_RM(model = mod, stochastic = FALSE, f = f, q = q, eip = eip, p = p, psi = diag(n_patch), nu = 25, M = M, Y = Y, Z = Z)
 setup_humans_SIP(model = mod, stochastic = FALSE, theta = TaR, SIP = human_state, b = b, c = c, r = r, rho = rho, eta = eta)
-setup_alternative_trace(model = mod)
-setup_visitor_trace(model = mod)
 
 # human output table
 human_out <- data.table::CJ(day = 1:tmax, state = c('S', 'I', 'P'), patch = 1:n_patch, value = NaN)
@@ -245,9 +194,8 @@ pb <- progress_bar$new(total = tmax)
 
 # run it
 while (get_tnow(mod) <= tmax) {
-  
-  compute_bloodmeal(model = mod)
-  
+  compute_bloodmeal_simple(model = mod)
+
   step_aqua(model = mod)
   step_mosquitoes(model = mod)
   step_humans(model = mod)
@@ -264,17 +212,80 @@ while (get_tnow(mod) <= tmax) {
   pb$tick()
 }
 
-human_summarized <- human_out[, .(value = sum(value)), by = .(state, day)]
-
-ggplot(data = human_summarized) +
-  geom_line(aes(x = day, y = value, color = state)) +
-  facet_wrap(. ~ state, scales = "free") +
-  ggtitle("deterministic humans")
-
+human_out <- human_out[, .(value = sum(value)), by = .(state, day)]
 mosy_out <- mosy_out[, .(value = sum(value)), by = .(state, day)]
 
-ggplot(mosy_out) +
-  geom_line(aes(x = day, y = value, color = state)) +
-  facet_wrap(. ~ state, scales = "free") +
-  ggtitle("deterministic mosy")
+human_out[, species := "human"]
+mosy_out[, species := "mosquito"]
+det_out <- rbind(human_out, mosy_out)
+
+
+# --------------------------------------------------------------------------------
+#   stochastic simulation
+# --------------------------------------------------------------------------------
+
+parout <- parallel::mclapply(X = 1:40, FUN = function(runid) {
+  
+  mod <- make_MicroMoB(tmax = tmax, p = n_patch)
+  
+  human_state <- round(human_state)
+  M <- round(M)
+  Y <- round(Y)
+  Z <- round(Z)
+  
+  setup_aqua_trace(model = mod, lambda = lambda, stochastic = TRUE)
+  setup_mosquito_RM(model = mod, stochastic = TRUE, f = f, q = q, eip = eip, p = p, psi = diag(n_patch), nu = 25, M = M, Y = Y, Z = Z)
+  setup_humans_SIP(model = mod, stochastic = TRUE, theta = TaR, SIP = human_state, b = b, c = c, r = r, rho = rho, eta = eta)
+  
+  # human output table
+  human_out <- data.table::CJ(day = 1:tmax, state = c('S', 'I', 'P'), patch = 1:n_patch, value = NaN)
+  human_out <- human_out[c('S', 'I', 'P'), on="state"]
+  data.table::setkey(human_out, day, patch)
+  
+  # mosy output table
+  mosy_out <- data.table::CJ(day = 1:tmax, state = c('M', 'Y', 'Z'), patch = 1:n_patch, value = NaN)
+  mosy_out <- mosy_out[c('M', 'Y', 'Z'), on="state"]
+  data.table::setkey(mosy_out, day, patch)
+  
+  # run it
+  while (get_tnow(mod) <= tmax) {
+    
+    compute_bloodmeal_simple(model = mod)
+
+    step_aqua(model = mod)
+    step_mosquitoes(model = mod)
+    step_humans(model = mod)
+    
+    human_out[day==get_tnow(mod) & state=='S', value := mod$human$SIP[, 'S']]
+    human_out[day==get_tnow(mod) & state=='I', value := mod$human$SIP[, 'I']]
+    human_out[day==get_tnow(mod) & state=='P', value := mod$human$SIP[, 'P']]
+    
+    mosy_out[day == get_tnow(mod) & state == 'M', value := mod$mosquito$M]
+    mosy_out[day == get_tnow(mod) & state == 'Y', value := mod$mosquito$Y]
+    mosy_out[day == get_tnow(mod) & state == 'Z', value := mod$mosquito$Z]
+    
+    mod$global$tnow <- mod$global$tnow + 1L
+  }
+  
+  human_out <- human_out[, .(value = sum(value)), by = .(state, day)]
+  mosy_out <- mosy_out[, .(value = sum(value)), by = .(state, day)]
+  
+  human_out[, species := "human"]
+  mosy_out[, species := "mosquito"]
+  
+  out <- rbind(mosy_out, human_out)
+  out[, run := as.integer(runid)]
+  
+  return(out)
+}, mc.cores = 10)
+
+parout <- data.table::rbindlist(parout)
+
+sto_mean <- parout[, .('mean' = mean(value)), by = .(state, day, species)]
+
+ggplot(parout) +
+  geom_line(aes(x = day, y = value, color = species, linetype = state, group = run), alpha = 0.1) +
+  geom_line(data = det_out, aes(x = day, y = value, color = species)) +
+  geom_line(data = sto_mean, aes(x = day, y = mean, color = species)) +
+  facet_wrap(species ~ state, scales = "free")
 
